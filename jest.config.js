38dd313f0ef6889baa42fc6e74c76496dc4e6375
roman-usov <66104246+roman-usov@users.noreply.github.com>
2022-06-19 12:49:55 +0300

    updated package.json to have happy-dom packages

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
-  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.test.js'],
+  testEnvironment: '@happy-dom/jest-environment',
+  coveragePathIgnorePatterns: [],
+  // setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.js'],
 };
